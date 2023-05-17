f1d4b642e01033ec838fca20ec3298b578e9375c
arafat-sani <ffsani029@gmail.com>
2023-05-17 23:54:25 +0600

    footer added

--- src/page/Shared/Footer/Footer.jsx
+++ src/page/Shared/Footer/Footer.jsx
@@ -1,9 +1,47 @@
+import { Link } from "react-router-dom";
+import logo from '../../../assets/logo.svg';
+import { FaFacebookF, FaInstagram, FaTwitter, FaTelegramPlane} from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>
-            <h2>footer</h2>
+     <div>
+    <footer className="footer p-10 bg-base-200 text-base-content orange-bg">
+    <div>
+        <span className="footer-title">Services</span>
+        <Link to='' className="link link-hover">Home Delevary</Link>
+        <Link to='' className="link link-hover">All Sports Car Toy</Link>
+        <Link to='' className="link link-hover">24h Service</Link>
+        <Link to='' className="link link-hover">Company</Link>
+    </div> 
+    <div>
+        <span className="footer-title">Company</span> 
+        <Link to='' className="link link-hover">About us</Link>
+        <Link to='' className="link link-hover">Contact</Link>
+        <Link to='' className="link link-hover">Jobs</Link>
+        <Link to='' className="link link-hover">Press kit</Link>
+    </div> 
+    <div>
+        <span className="footer-title">Legal</span> 
+        <Link to='' className="link link-hover">Terms of use</Link>
+        <Link to='' className="link link-hover">Privacy policy</Link>
+        <Link to='' className="link link-hover">Cookie policy</Link>
+    </div>
+    </footer> 
+    <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300 orange-bg">
+    <div className="items-center grid-flow-col">
+        <img src={logo} alt="" />
+        <p>Toy shop Ltd. <br/>Providing reliable tech since 1992</p>
+    </div> 
+    <div className="md:place-self-center md:justify-self-end">
+        <div className="grid grid-flow-col gap-4">
+            <Link to='' className="text-lg"><FaFacebookF></FaFacebookF></Link>
+            <Link to='' className="text-lg"><FaInstagram></FaInstagram></Link>
+            <Link to='' className="text-lg"><FaTwitter></FaTwitter></Link>
+            <Link to='' className="text-lg"><FaTelegramPlane></FaTelegramPlane></Link>
         </div>
+    </div>
+    </footer>
+     </div> 
     );
 };
 
