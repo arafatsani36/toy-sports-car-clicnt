711348e63763fbba2d6b051f408a0d05cad747af
arafat-sani <ffsani029@gmail.com>
2023-05-22 19:47:57 +0600

    firebase key hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUZxFpxbn_d2JDHxe7vWxwwHTAaPuxtKo",
-  authDomain: "toy-sports-car.firebaseapp.com",
-  projectId: "toy-sports-car",
-  storageBucket: "toy-sports-car.appspot.com",
-  messagingSenderId: "523637698163",
-  appId: "1:523637698163:web:08fecaef9cffd180b84406"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
